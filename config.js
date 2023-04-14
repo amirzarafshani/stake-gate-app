a727c633a713721c8ab5c3bb7b66c4c65eb894bb
unknown <amirzarafshani@gmail.com>
2023-04-14 08:59:36 +0300

    changing config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://api.diamond-stake.com";
-export const BASE_URL = "http://212.253.87.46:3003";
+export const BASE_URL = "https://api.diamond-stake.com";
 // export const BASE_URL = "http://138.68.99.46:4004";
 export const DEPOSIT_WALLET_ADDRESS = "TC3HwYZ21kyFPyLJ6aSvYexvuYPm9cdugk";
